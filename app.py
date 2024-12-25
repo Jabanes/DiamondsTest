088ade804b81699e3903cce7e23a6b3c0611936c
Jabanes <erezhabani2003@gmail.com>
2024-12-25 14:04:52 +0200

    fixed bugs, added input validations

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from enum import Enum
-import statistics
+import os
+import platform
+
 df = pd.read_csv('diamonds.csv')
 
 class Options(Enum):
@@ -12,8 +14,11 @@ class Options(Enum):
     MEDIAN_PREMIUM = 5
     AVARAGE_PER_CUT = 6
     AVARAGE_PER_COLOR = 7
+    EXIT = 8
 
 def printMenu():
+    print("------------------------------")
+    print("MENU")
     for option in Options:
         print(f"{option.value} - {option.name}")
 
@@ -24,7 +29,7 @@ def mostExpensive():
 
 def avaragePrice():
     avarage = df["price"].mean()
-    print(f"The avarage price of aa diamond is: {avarage:.2f}$")
+    print(f"The avarage price of a diamond is: {avarage:.2f}$")
 
 def idealCount():
     count = 0
@@ -44,49 +49,93 @@ def countColors():
 
 def medianPremium():
     filteredCaratForPremium = df[df['cut']=='Premium']['carat'].median()
-    print(filteredCaratForPremium)
+    print(f"The Median carat for Premium cut is: {filteredCaratForPremium} carat")
 
 def avaragePerCut():
     cuts = list(df['cut'].unique())
     for index,cut in enumerate(cuts):
-        print(f"{index}: {cut}")
-    cut_selection = input("select a cut to find avarage for (by name): ")
-    
-    avarageForCut = df[df['cut']== cut_selection]['carat'].mean()
-    print(f"The avarage carat for {cut_selection} is {avarageForCut :.2f}")
+        print(f"{index}: '{cut}'")
+    try:
+        cut_selection = input("Select a cut to find avarage carat for (by name): ").title()
+        if cut_selection.isdigit() == False:
+            
+            if cut_selection == "":
+                print("Please give me something to work with...")
+            
+            elif cut_selection not in cuts:
+                print(f"Couldn't find a cut by the name of: {cut_selection}")
+            
+            else:
+                avarageForCut = df[df['cut']== cut_selection]['carat'].mean()
+                print(f"The avarage carat for {cut_selection} is {avarageForCut :.2f} carat")
+                
+        else:
+            print("Select by name, not index!")
+            
+
+    except ValueError:
+        print("Make sure to type in the exact same names for cuts!")
     
 def avaragePerColor():
     colors = list(df['color'].unique())
-    for index,color in enumerate(colors):
-        print(f"{index}: {color}")
-    color_selection = input("select a cut to find avarage for (by name): ")
+    for color in (colors):
+        print(f"color: '{color}'")
+    try:
+        color_selection = input("select a color to find avarage price for (by name): ").capitalize()
 
-    avarageForColor = df[df['color']== color_selection]['price'].mean()
-    print(f"The avarage price for  color: '{color_selection}' is {avarageForColor :.2f}")
+        if color_selection.isdigit() == False:
+
+            if color_selection == "":
+                    print("Please give me something to work with...")
+
+            elif color_selection not in colors:
+                    print(f"Couldn't find a color by the name of: {color_selection}")
+
+            else:
+                avarageForColor = df[df['color']== color_selection]['price'].mean()
+                print(f"The avarage price for  color: '{color_selection}' is {avarageForColor :.2f}$")
+            
+        else:
+            print("Select by name, not index!")
     
+        
+
+    except ValueError:
+        print("Invalid option!")
+
+def clear_terminal():
+    if platform.system() == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
 
 if __name__ == "__main__":
+   
+    while True:
+        try:
+            printMenu()
+            user_selection = input("Select an option (by index): ")
+            clear_terminal()
+            if user_selection.isdigit():
+                user_selection = Options(int(user_selection))
 
-    printMenu()
-    user_selection = input("Select an option (by index): ")
-
-    if user_selection.isdigit():
-        user_selection = Options(int(user_selection))
-
-        if user_selection == Options.MOST_EXPENSIVE:
-            mostExpensive()
-        if user_selection == Options.AVARAGE_PRICE:
-            avaragePrice()
-        if user_selection == Options.IDEAL:
-            idealCount()
-        if user_selection == Options.COLORS:
-            countColors()
-        if user_selection == Options.MEDIAN_PREMIUM:
-            medianPremium()
-        if user_selection == Options.AVARAGE_PER_CUT:
-            avaragePerCut()
-        if user_selection == Options.AVARAGE_PER_COLOR:
-            avaragePerColor()
-
-    else:
-        print("invalid option!")
+                if user_selection == Options.MOST_EXPENSIVE:
+                    mostExpensive()
+                if user_selection == Options.AVARAGE_PRICE:
+                    avaragePrice()
+                if user_selection == Options.IDEAL:
+                    idealCount()
+                if user_selection == Options.COLORS:
+                    countColors()
+                if user_selection == Options.MEDIAN_PREMIUM:
+                    medianPremium()
+                if user_selection == Options.AVARAGE_PER_CUT:
+                    avaragePerCut()
+                if user_selection == Options.AVARAGE_PER_COLOR:
+                    avaragePerColor()
+                if user_selection == Options.AVARAGE_PER_COLOR:
+                    exit()
+            else:
+                print("invalid option!")
+        except ValueError:
+            print("Out of range!")
